59cf5cce052e001bdd1e577a0075c5d34dc6d34a
Annisa Sucianty <annisasucianty70@gmail.com>
2019-10-24 02:03:45 -0700

    UTS-DASAR PEMROGRAMAN

---
+++ BAB I/ProgramKalkulator/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "2 + 2 = " << 2+2 << endl << endl;
+    cout << "20 - 7= " << 20-7 << endl << endl;
+    cout << "5 x 5 = " << 5 * 5 << endl << endl;
+    cout << "36 / 3= " << 36/3 << endl << endl;
+
+    return 0;
+}
---
+++ BAB I/biodatalangsung/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama Lengkap   : Annisa Sucianty Aulya Suganda" << endl;
+    cout << "Kelas          : TI-B" << endl;
+    cout << "Umur           : 18 Tahun" << endl;
+    cout << "TTL            : Bandung, 22 Juni 2001" <<
+
+    return 0;
+}
